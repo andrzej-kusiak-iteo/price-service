ad89892c4057ee4d66aa568a7ae6f5f855c896df
Andrzej Kusiak <andrzej.kusiak@iteo.com>
2023-01-03 16:24:02 +0100

    Adding db structure

---
+++ src/main/resources/liquibase/sql/APP_DDL.sql
@@ -0,0 +1,20 @@
+create table products
+(
+    product_id uuid primary key default gen_random_uuid(),
+    name       text          not null,
+    price      numeric(9, 2) not null
+);
+
+create table discounts
+(
+    discount_id   serial primary key,
+    product_id    uuid not null references products (product_id),
+    amount_factor integer,
+    percentage    integer,
+    check ( -- either amount or percentage based discount type
+            (amount_factor is not null and percentage is null)
+            or (amount_factor is null and percentage is not null)
+        )
+);
+
+create index discounts_product_id_idx on discounts (product_id);
